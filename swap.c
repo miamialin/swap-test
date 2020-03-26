91b96c4fa8a1d4cbcf51c2688ec58f9601af743b
miamialin <mylsonge@gmail.com>
2020-03-26 17:09:53 +0800

    print x y

---
+++ swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x = 28;
+	int y = 50;
+
+	printf("x is %i\n");
+	printf("y is %i\n");
+
+	// swapping...
+
+	printf("x is now %i\n");
+	printf("y is now %i\n");
+
+	return 0;
+}
